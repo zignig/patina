34ec0596d4042f6b6722d3c21053a3a3a1e2ebc1
Simon Kirkby <obeygiantrobot@gmail.com>
2024-03-06 07:55:24 +0800

    rebuild of the fabric

--- tinybxtest.py
+++ tinybxtest.py
@@ -16,6 +16,7 @@ from hapenny.cpu import Cpu
 from hapenny.bus import BusPort, SimpleFabric, partial_decode
 from hapenny.serial import BidiUart
 from hapenny.mem import BasicMemory
+from hapenny.gpio import OutputPort, InputPort
 
 from warmboot import WarmBoot
 from twiddler import Twiddle
@@ -31,52 +32,54 @@ boot_image = struct.unpack("<" + "h" * (len(bootloader) // 2), bootloader)
 
 class Computer(Elaboratable):
     def __init__(self):
+        F = 16e6  # Hz
         super().__init__()
+        self.cpu = Cpu(reset_vector=4096, addr_width=16)
+
+        self.mainmem = BasicMemory(depth=256 * 30)
+        self.bootmem = BasicMemory(
+            depth=RAM_WORDS, read_only=True, contents=boot_image, name="boot"
+        )
+        self.bidi = BidiUart(baud_rate=57600, oversample=8, clock_freq=F)
+        self.led = OutputPort(1, read_back=True)
+        self.input = InputPort(1)
+
+        self.cpu.add_component(
+            [self.mainmem, self.bootmem, self.bidi, self.led]
+        )
+        #self.cpu.add_component([self.bootmem, self.led])
+        
+        self.memory_map = self.cpu.memory_map
 
     def elaborate(self, platform):
         m = Module()
-        F = 16e6  # Hz
 
-        m.submodules.cpu = cpu = Cpu(
-            # execute from the bootloader dump.
-            reset_vector=4096,
-            addr_width=14,
-        )
-        m.submodules.mainmem = mainmem = BasicMemory(depth=256 * 16)
-        m.submodules.mem = bootmem = BasicMemory(depth=RAM_WORDS, contents=boot_image)
-        m.submodules.uart = uart = BidiUart(
-            baud_rate=57600, oversample=8, clock_freq=F
-        )
+        self.cpu.build(m)
 
-        m.submodules.iofabric = iofabric = SimpleFabric(
-            [
-                partial_decode(m, bootmem.bus, 11),  # 0x____0000
-                partial_decode(m, uart.bus, 11),  # 0x____1000
+        uart = False
+        led = True
+
+        if uart:
+            uartpins = platform.request("uart", 0)
+        
+            rx_post_sync = Signal()
+
+            m.submodules.rxsync = amaranth.lib.cdc.FFSynchronizer(
+                i=uartpins.rx.i,
+                o=rx_post_sync,
+                o_domain="sync",
+                init=1,
+                stages=2,
+            )
+
+            m.d.comb += [
+                uartpins.tx.o.eq(self.bidi.tx),
+                self.bidi.rx.eq(rx_post_sync),
             ]
-        )
-        m.submodules.fabric = fabric = SimpleFabric(
-            [
-                mainmem.bus,
-                partial_decode(m, iofabric.bus, 12), 
-            ]
-        )
 
-        connect(m, cpu.bus, fabric.bus)
-
-        uartpins = platform.request("uart", 0)
-
-        rx_post_sync = Signal()
-        m.submodules.rxsync = amaranth.lib.cdc.FFSynchronizer(
-            i=uartpins.rx.i,
-            o=rx_post_sync,
-            o_domain="sync",
-            init=1,
-            stages=2,
-        )
-        m.d.comb += [
-            uartpins.tx.o.eq(uart.tx),
-            uart.rx.eq(rx_post_sync),
-        ]
+        if led:
+            user = platform.request("boot", 0)
+            m.d.comb += user.o.eq(self.led.pins[0])
 
         # # Attach the warmboot
         # m.submodules.warm = warm = WarmBoot()
@@ -90,8 +93,7 @@ class Computer(Elaboratable):
         #     warm.user.eq(user.i)
         # ]
 
-        m.submodules.twiddle = tw = Twiddle()
-
+        # m.submodules.twiddle = tw = Twiddle()
 
         return m
 
@@ -104,12 +106,15 @@ p.add_resources(
         UARTResource(
             0, rx="A8", tx="B8", attrs=Attrs(IO_STANDARD="SB_LVCMOS", PULLUP=1)
         ),
-        Resource("boot", 0, Pins("A9", dir="i"), Attrs(IO_STANDARD="SB_LVCMOS")),
+        Resource("boot", 0, Pins("A9", dir="o"), Attrs(IO_STANDARD="SB_LVCMOS")),
         Resource("user", 0, Pins("H2", dir="o"), Attrs(IO_STANDARD="SB_LVCMOS")),
     ]
 )
 
-p.build(Computer(),do_program=True)
+
+pooter = Computer()
+pooter.cpu.show()
+p.build(pooter, do_program=True)
 
 # TINYBOOT_UART_ADDR=12288 cargo objcopy --release -- -O binary ../tinybx8k.bin
 # MEMORY {
