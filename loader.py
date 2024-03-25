5840ab8929bdf766c1a72fd873aa08d3cdff5ec0
Simon Kirkby <obeygiantrobot@gmail.com>
2024-03-25 23:47:19 +0800

    loader nearly working

--- loader.py
+++ loader.py
@@ -4,12 +4,25 @@
 
 from serial.tools.miniterm import Miniterm
 import serial
-import time
+
+from enum import Enum
+from pathlib import Path
+import struct
+
+
 
 the_port ="/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_A700eCzo-if00-port0"
 
-class Console:
-    def __init__(self, port=the_port, baud=57600):
+class Commands(Enum):
+    call = 0
+    write = 1
+    read = 2
+    load_a  = 3
+    load_c = 4
+    ping = 5
+
+class MonTool:
+    def __init__(self, port, baud=57600):
         self.port = port
         self.baud = baud
         self.ser = serial.serial_for_url(
@@ -32,27 +45,82 @@ class Console:
         print("Attach console")
         term.start()
         term.join(True)
-
-class MonTool:
-    def __init__(self,port):
-        self.port = port
     
-    def ping(self):
-        self.port.write([5])
-        val = self.port.read()
+    def _cmd(self,cmd):
+        self.ser.write([cmd.value])
+    
+    def _ack(self):
+        val = self.ser.read()
         if val[0] == 0xAA: # ack packet
             return True
         else:
-            return False
+            raise("fail")
+
+    def _write_num(self,val):
+        num = val.to_bytes(4,byteorder="little")
+        self.ser.write(num)
     
+    def _read_num(self):
+        data = self.ser.read(4)
+        num = int.from_bytes(data,byteorder="little")
+        return num
 
-    def call(self):
-        self.port.write([0])
+    def _write_a(self,val):
+        self._cmd(Commands.load_a)
+        self._write_num(val)
 
-    def _load_addr(self):
-        self.port.write([3])
-        
+    def _write_c(self,val):
+        self._cmd(Commands.load_c)
+        self._write_num(val)
+
+    def ping(self):
+        self._cmd(Commands.ping)
+        self._ack()
+
+    def call(self,addr):
+        self._write_a(addr)
+        self._ack()
+        self._cmd(Commands.call)
+        # does not return
+
+    def read(self,addr,count):
+        self._write_a(addr)
+        self._ack()
+        self._write_c(count)
+        self._ack()
+        self._cmd(Commands.read)
+        self._ack()
+        data = []
+        for pos in range(count):
+            val = self._read_num()
+            data.append(val)
+        return data
+
+    def write(self,addr,data):
+        count = len(data)
+        print(addr,count)
+        self._write_a(addr)
+        self._ack()
+        self._write_c(count)
+        self._ack()
+        self._cmd(Commands.write)
+        for val in data:
+            self._write_num(val)
+        self._ack()
+
+    def peek(self,addr):
+        return self.read(addr,1)
+
+    def poke(self,addr,val):
+        self.write(addr,val)
+
+    def load(self,file_name):
+        bootloader = Path(file_name).read_bytes()
+        boot_image = struct.unpack("<" + "I" * (len(bootloader) // 4), bootloader)
+        return list(boot_image)
+
+
 if __name__ == "__main__":
-    c = Console()
-    m = MonTool(c.ser)
+    m = MonTool(the_port)
     m.ping()
+    m.load("tinybx8k.bin")
